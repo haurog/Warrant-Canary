17fb1bd0af6dbf64f733f68c3d0daacc8464102e
Roger <rhaeuser@netlink.ch>
2021-10-19 21:14:03 +0200

    Added mapping for owner to ids and trusted third parties to ids

--- contracts/WarrantCanary.sol
+++ contracts/WarrantCanary.sol
@@ -21,7 +21,11 @@ contract WarrantCanaray {
         uint enclosedFunds;
     }
 
-    mapping(uint => warrantCanary)  warrantCanaries;
+    mapping(uint => warrantCanary)  warrantCanaries;  // All warrant canaries by ID
+
+    mapping(address => uint[]) IDsOwned;  // to store all warrant canaries that an address has
+
+    mapping(address => uint[]) IDsTrusted;  // to store all warrant canaries that have this trusted third party.
 
 
     modifier onlyCanaryOwner() {
