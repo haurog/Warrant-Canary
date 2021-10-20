33b9b36eda4584758b23b7f9507f1b20ab3fd411
haurog <36535774+haurog@users.noreply.github.com>
2021-10-20 21:11:21 +0200

    Added a test file (no writted tests yet)

---
+++ test/warrant_canary.js
@@ -0,0 +1,13 @@
+const WarrantCanary = artifacts.require("WarrantCanary");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("WarrantCanary", function (/* accounts */) {
+  it("should assert true", async function () {
+    await WarrantCanary.deployed();
+    return assert.isTrue(true);
+  });
+});
