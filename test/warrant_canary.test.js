c06c3a69911e0a37ff59102c0a79ff769ac8c533
haurog <36535774+haurog@users.noreply.github.com>
2021-10-21 20:47:55 +0200

    extended tests

--- test/warrant_canary.test.js
+++ test/warrant_canary.test.js
@@ -5,9 +5,12 @@ const WarrantCanary = artifacts.require("WarrantCanary");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("WarrantCanary", function (/* accounts */) {
+contract("WarrantCanary", function (accounts) {
   it("should assert true", async function () {
     await WarrantCanary.deployed();
     return assert.isTrue(true);
   });
+  // it("should return the list of accounts", async ()=> {
+  //   console.log(accounts);
+  // });
 });
