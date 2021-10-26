c92766c5aecd78c27ca9af2e3d86eb4c1e378d62
haurog <36535774+haurog@users.noreply.github.com>
2021-10-26 20:20:14 +0200

    simplyfied tests

--- test/warrant_canary.test.js
+++ test/warrant_canary.test.js
@@ -20,62 +20,47 @@ contract("WarrantCanary", function (accounts) {
 
   describe("Create warrant canary contract.", () => {
     it("should emit a Log and store all elements correctly when a simple contract is created.", async () => {
-      let eventEmitted = false;
 
-      console.log("first log: " + createTx.logs[0]);
-      if (createTx.logs[0].event == "LogCreated") {
-        eventEmitted = true;
-      }
-
-      assert.equal(
-        eventEmitted,
-        true,
+      assert(
+        createTx.logs[0].event == "LogCreated",
         "creating a warrant canary should emit a Created event",
       );
 
       const result = await instance.warrantCanaries.call(0);
-      console.log("Purpose: " + result.purpose);
+      // console.log("Purpose: " + result.purpose);
 
-      assert.equal(
-        result.purpose,
-        purpose,
+      assert(result.purpose == purpose,
         "The purpose is not stored correctly",
       );
-      assert.equal(
-        result.expirationBlock,
-        expirationBlock,
+      assert(result.expirationBlock == expirationBlock,
         "The expirationBlock is not stored properly.",
       );
-      assert.equal(
-        result.warrantCanaryOwner,
-        accounts[0],
+
+      assert(result.warrantCanaryOwner == accounts[0],
         "The owner of the warrant Canary is not set properly",
       );
     });
-    it("should emit a Log when funds are withdrawn", async () => {
-      let eventEmitted = false;
-      const purpose = "test the contract."
-      const expirationBlock = 111;
-      await instance.createWarrantCanarySimple(expirationBlock, purpose);
 
+    it("should emit a Log when funds are added and withdrawn", async () => {
       const addFundsTx = await instance.addFunds(0, {value: 250});
-
       const withdrawTx = await instance.withdrawSomeFunds(0, 200);
 
       // console.log("test: " + withdrawTx.logs[0].event);
 
-      if (withdrawTx.logs[0].event == "LogFundsWithdrawn") {
-        eventEmitted = true;
-      }
+      assert(
+        addFundsTx.logs[0].event == "LogFundsAdded",
+        "adding funds should emit an event",
+      );
 
-      assert.equal(
-        eventEmitted,
-        true,
+      assert(
+        withdrawTx.logs[0].event == "LogFundsWithdrawn",
         "Withdrawing funds should emit an event",
       );
 
-      // const result = await instance.warrantCanaries.call(0);
-      // console.log(result.purpose);
+      assert(
+        withdrawTx.logs[0].event == "LogFundsWithdrawn",
+        "Withdrawing funds should emit an event",
+      );
     });
   });
 
