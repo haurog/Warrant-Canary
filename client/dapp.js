a608de834853ffd8fa69b029a4f0bebe59b6ec30
haurog <36535774+haurog@users.noreply.github.com>
2021-10-28 20:45:14 +0200

    website basics are working for a simple creation of a warrant canary (nothing else)

--- client/dapp.js
+++ client/dapp.js
@@ -1,39 +1,399 @@
 
+// import WarrantCanary from './../build/contracts/WarrantCanary.json'
+
 
 // contract address on Ropsten:
-const ssAddress = '0x295eb38E3660d440B1dd54822644E545635f44E1'
+// const ssAddress = '0x295eb38E3660d440B1dd54822644E545635f44E1'
+const wcAddress = '0xd104E11d5663cca951e023b7873689641e24dc96'
 
 // add contract ABI from Remix:
 
-const ssABI =
+// fetch("./../build/contracts/WarrantCanary.json")
+// .then(response => {
+//    return response.json();
+// })
+// .then(data => console.log(data));
+
+const wcABI =
 [
-	{
-		"inputs": [],
-		"name": "retrieve",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "num",
-				"type": "uint256"
-			}
-		],
-		"name": "store",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "warrantCanaryID",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "oldTrustedThirdParty",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "newTrustedThirdParty",
+        "type": "address"
+      }
+    ],
+    "name": "LogChangedTrustedThirdParty",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "warrantCanaryID",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "purpose",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "trustedThirdParty",
+        "type": "address"
+      }
+    ],
+    "name": "LogCreated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "warrantCanaryID",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "oldExpirationBlock",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "newExpirationBlock",
+        "type": "uint256"
+      }
+    ],
+    "name": "LogExpirationUpdated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "warrantCanaryID",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "LogFundsAdded",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "warrantCanaryID",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "LogFundsWithdrawn",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "IDcount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "IDsOwned",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "IDsTrusted",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "warrantCanaries",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "ID",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "expirationBlock",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "lastUpdatedInBlock",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "purpose",
+        "type": "string"
+      },
+      {
+        "internalType": "address payable",
+        "name": "warrantCanaryOwner",
+        "type": "address"
+      },
+      {
+        "internalType": "address payable",
+        "name": "trustedThirdParty",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "enclosedFunds",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "expirationBlock_",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "purpose_",
+        "type": "string"
+      },
+      {
+        "internalType": "address payable",
+        "name": "trustedThirdParty_",
+        "type": "address"
+      }
+    ],
+    "name": "createWarrantCanary",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "expirationBlock_",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "purpose_",
+        "type": "string"
+      }
+    ],
+    "name": "createWarrantCanarySimple",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "warrantCanaryID_",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "newExpirationBlock_",
+        "type": "uint256"
+      }
+    ],
+    "name": "updateExpiration",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "warrantCanaryID_",
+        "type": "uint256"
+      }
+    ],
+    "name": "addFunds",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "warrantCanaryID_",
+        "type": "uint256"
+      },
+      {
+        "internalType": "address payable",
+        "name": "newTrustedThirdParty_",
+        "type": "address"
+      }
+    ],
+    "name": "changeTrustedThirdParty",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "warrantCanaryID_",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "fundsToWithdraw_",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdrawSomeFunds",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "warrantCanaryID_",
+        "type": "uint256"
+      }
+    ],
+    "name": "withdrawAllFunds",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "warrantCanaryID_",
+        "type": "uint256"
+      }
+    ],
+    "name": "deleteWarrantCanary",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "warrantCanaryID_",
+        "type": "uint256"
+      }
+    ],
+    "name": "Expiration",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+let response = await fetch("./../build/contracts/WarrantCanary.json");
+
+console.log(response.json());
+
+// const wc = JSON.parse(fs.readFileSync('./build/contracts/WarrantCanary.json', 'utf8'));
+// console.log(JSON.stringify(contract.abi));
 
 // Using the 'load' event listener for Javascript to
 // check if window.ethereum is available
@@ -90,28 +450,37 @@ mmEnable.onclick = async () => {
 
 // grab the button for input to a contract:
 
-const ssSubmit = document.getElementById('ss-input-button');
+const createButton = document.getElementById('create-button');
 
-ssSubmit.onclick = async () => {
+createButton.onclick = async () => {
   // grab value from input
 
-  const ssInputValue = document.getElementById('ss-input-box').value;
-  console.log(ssInputValue)
+  const createExpirationInput = document.getElementById('create-button-expiration-input').value;
+  console.log(createExpirationInput);
+  const createPurposeInput = document.getElementById('create-button-purpose-input').value;
+  console.log(createPurposeInput);
+  const createtrustedThirdPartyInput = document.getElementById('create-button-trustedThirdParty-input').value;
+  console.log(createtrustedThirdPartyInput);
+
+  console.log(createExpirationInput, createPurposeInput)
 
   var web3 = new Web3(window.ethereum)
 
   // instantiate smart contract instance
 
-  const simpleStorage = new web3.eth.Contract(ssABI, ssAddress)
-  simpleStorage.setProvider(window.ethereum)
+  const warrantCanary = new web3.eth.Contract(wcABI, wcAddress)
+  warrantCanary.setProvider(window.ethereum)
 
-  await simpleStorage.methods.store(ssInputValue).send({from: ethereum.selectedAddress})
+  await warrantCanary.methods.createWarrantCanarySimple(
+    createExpirationInput,
+    createPurposeInput
+    ).send({from: ethereum.selectedAddress})
 
 }
 
-const ssGetValue = document.getElementById('ss-get-value')
+const getExpirationButton = document.getElementById('getExpiration-button')
 
-ssGetValue.onclick = async () => {
+getExpirationButton.onclick = async () => {
 
   var web3 = new Web3(window.ethereum)
 
