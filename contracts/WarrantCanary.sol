d1ec8942c04be2dee5edbd9b2b71411b868fbe82
haurog <36535774+haurog@users.noreply.github.com>
2021-11-16 12:01:48 +0100

    Added NatSPec comments

--- contracts/WarrantCanary.sol
+++ contracts/WarrantCanary.sol
@@ -13,16 +13,16 @@ contract WarrantCanary is Ownable, Pausable {
     struct warrantCanary {
         uint ID;
         uint expirationTime;
-        uint lastUpdatedInBlock;  // tracks in which block expiration or trusted third party has been changed
+        uint lastUpdatedInBlock;  // tracks in which block expiration or trusted third party has been changed.
         string purpose;
         address payable warrantCanaryOwner;
         address payable trustedThirdParty;
         uint enclosedFunds;
     }
 
-    mapping(uint => warrantCanary) public  warrantCanaries;  // All warrant canaries accessed by IDs
-    mapping(address => uint[]) public IDsOwned;  // Store all warrant canaries that an address owns
-    mapping(address => uint[]) public IDsTrusted;  // Store all warrant canaries that have this address as a trusted third party
+    mapping(uint => warrantCanary) public  warrantCanaries;  // All warrant canaries accessed by IDs.
+    mapping(address => uint[]) public IDsOwned;  // Store all warrant canaries that an address owns.
+    mapping(address => uint[]) public IDsTrusted;  // Store all warrant canaries that have this address as a trusted third party.
 
     event LogCreated(uint warrantCanaryID, string purpose,address trustedThirdParty);
     event LogExpirationUpdated(uint warrantCanaryID, uint oldExpirationBlock, uint newExpirationBlock);
@@ -56,9 +56,9 @@ contract WarrantCanary is Ownable, Pausable {
         _;
     }
 
-    /// @notice Creates a new Warrant Canary with trusted thirdParty (can be set to 0x0)
-    /// @param expirationTime_: The time (unix epoch in seconds) when the warrant canary expires
-    /// @param purpose_: A string describing the purpose of the warrant canary
+    /// @notice Creates a new Warrant Canary with trusted thirdParty (can be set to 0x0).
+    /// @param expirationTime_: The time (unix epoch in seconds) when the warrant canary expires.
+    /// @param purpose_: A string describing the purpose of the warrant canary.
     /// @param trustedThirdParty_: An address of a trusted third party. Can be 0x0 if a plain warrant canary should be used.
     function createWarrantCanary(
         uint expirationTime_,
@@ -93,9 +93,9 @@ contract WarrantCanary is Ownable, Pausable {
 
     }
 
-    /// @notice Update the expiration time for an owned warrant canary contract
-    /// @param warrantCanaryID_: ID (uint) of the warrant canary whose expiration time should be changed
-    /// @param newExpirationTime_: The time (unix epoch in seconds) when the warrant canary expires
+    /// @notice Update the expiration time for an owned warrant canary contract.
+    /// @param warrantCanaryID_: ID (uint) of the warrant canary whose expiration time should be changed.
+    /// @param newExpirationTime_: The time (unix epoch in seconds) when the warrant canary expires.
     function updateExpiration(uint warrantCanaryID_, uint newExpirationTime_)
         public
         onlyCanaryOwner(warrantCanaryID_)
@@ -106,8 +106,8 @@ contract WarrantCanary is Ownable, Pausable {
         emit LogExpirationUpdated(warrantCanaryID_, oldExpirationTime, newExpirationTime_);
     }
 
-    /// @notice Add funds to a warrant canary (ETH only)
-    /// @param warrantCanaryID_: ID (uint) of the warrant canary to which the funds are added
+    /// @notice Add funds to a warrant canary (ETH only).
+    /// @param warrantCanaryID_: ID (uint) of the warrant canary to which the funds are added.
     function addFunds(uint warrantCanaryID_)
     public
     payable
@@ -118,8 +118,8 @@ contract WarrantCanary is Ownable, Pausable {
         emit LogFundsAdded(warrantCanaryID_, msg.value);
     }
 
-    /// @notice Change the address of the trusted third party
-    /// @param warrantCanaryID_: ID (uint) of the warrant canary whose trusted third party should be changed
+    /// @notice Change the address of the trusted third party. The 0x0 address is ownly allowed if no funds are enclosed.
+    /// @param warrantCanaryID_: ID (uint) of the warrant canary whose trusted third party should be changed.
     function changeTrustedThirdParty(
         uint warrantCanaryID_,
         address payable newTrustedThirdParty_
@@ -138,6 +138,8 @@ contract WarrantCanary is Ownable, Pausable {
         emit LogChangedTrustedThirdParty(warrantCanaryID_, oldTrustedThirdParty, newTrustedThirdParty_);
     }
 
+    /// @notice Withdraw a part of the funds from the warrant canary
+    /// @param warrantCanaryID_: ID (uint) of the warrant canary whose funds should be withdrawn
     function withdrawSomeFunds(uint warrantCanaryID_, uint fundsToWithdraw_)
         public
         onlyCanaryOwnerOrTrustedThirdParty(warrantCanaryID_)
@@ -149,14 +151,18 @@ contract WarrantCanary is Ownable, Pausable {
         emit LogFundsWithdrawn(warrantCanaryID_, fundsToWithdraw_);
     }
 
+    /// @notice Withdraw all funds from the warrant canary.
+    /// @param warrantCanaryID_: ID (uint) of the warrant canary whose full funds should be withdrawn.
     function withdrawAllFunds(uint warrantCanaryID_)
         public
         onlyCanaryOwnerOrTrustedThirdParty(warrantCanaryID_)
     {
-        // Withdraws all funds from a warrant canary. Calls "withdrawSomeFunds" with fundsToWithDraw = warrantCanary.enclosedFunds
         withdrawSomeFunds(warrantCanaryID_, warrantCanaries[warrantCanaryID_].enclosedFunds);
     }
 
+    /// @notice Delete a given warrant canary. Deletion is only possible if no funds are enclosed.
+    /// @param warrantCanaryID_: ID (uint) of the warrant canary which should be deleted.
+    /// @dev The function also deletes the warrant canary ID from all IDsTrusted and IDsOwned.
     function deleteWarrantCanary(uint warrantCanaryID_)
         public
         onlyCanaryOwnerOrTrustedThirdParty(warrantCanaryID_) {
@@ -179,12 +185,14 @@ contract WarrantCanary is Ownable, Pausable {
         emit LogDeleted(warrantCanaryID_);
     }
 
+    /// @notice A helper function to remove an entry from an array by value.
+    /// @param array_: Array from which an entry should be removed.
+    /// @param valueToDelete_: Value of the entry to be deleted.
+    /// @dev Keeps the order of the non removed elements (more expensive and not really necessary in this project).
     function removeByValue(uint[] storage array_, uint valueToDelete_)
         private
         returns(uint[] storage)
     {
-        // Keeps the order of the non removed elements (more expensive and not really necessary in this project)
-
         uint index = 0;
         // find index of the value (is unique)
         for(; index <= array_.length && array_[index] != valueToDelete_ ; index++){}
@@ -199,6 +207,9 @@ contract WarrantCanary is Ownable, Pausable {
         return array_;
     }
 
+    /// @notice A getter function get an array of all owned warrant Canary IDs back.
+    /// @param wcOwner: Address of which all the owned IDs are returned.
+    /// @return an array (can be empty) with all owned warrant canaries.
     function getIDsOwned(address wcOwner)
         public
         view
@@ -207,6 +218,9 @@ contract WarrantCanary is Ownable, Pausable {
         return IDsOwned[wcOwner];
     }
 
+    /// @notice A getter function get an array of all warrant Canary IDs back where the address is a trusted third party.
+    /// @param wcTrusted: Address of which all the trusted third party warrant canaries are returned.
+    /// @return an array (can be empty) with all the IDs for which the address is a trusted third party.
     function getIDsTrusted(address wcTrusted)
         public
         view
@@ -215,22 +229,23 @@ contract WarrantCanary is Ownable, Pausable {
         return IDsTrusted[wcTrusted];
     }
 
+    /// @notice Updates the variable "lastUpdatedInBlock" in the warrant canary.
+    /// @param warrantCanaryID_: ID (uint) of the warrant canary which should be deleted.
     function updateLastUpdatedInBlock(uint warrantCanaryID_) internal {
         warrantCanaries[warrantCanaryID_].lastUpdatedInBlock = block.number;
     }
 
-
-    // The following functions are admin functions (owner of the contract only)
-    // Therefore they are very minimal without events emitted.
-
+    /// @notice Pauses the contract. Contract owner only, therefore very minimal function.
     function pauseContract() public onlyOwner() {
         _pause();
     }
 
+    /// @notice Unpauses the contract. Contract owner only, therefore very minimal function.
     function unpauseContract() public onlyOwner() {
         _unpause();
     }
 
+    /// @notice Retrieves Ether which is not assciated with any warrant Canary. Contract owner only, therefore very minimal function.
     function retrieveExcessFunds() public onlyOwner() {
         uint allEnclosedFunds;
         for(uint i = 0; i < IDcount; i++)
