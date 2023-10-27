00e8770863949d0da4e9d620f6fa004245113f8a
haurog <36535774+haurog@users.noreply.github.com>
2023-10-27 10:13:17 +0200

    Create hardhat deploy scrip using frame.sh

--- scripts/1_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function(deployer) {
-    const WarrantCanary = artifacts.require('./WarrantCanary.sol');
-    await deployer.deploy(WarrantCanary);
-    const contract = await WarrantCanary.deployed();
-    console.log("Contract address:", contract.address)
-};
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,17 @@
 const hre = require("hardhat");
+const ethProvider = require('eth-provider') // eth-provider is a simple EIP-1193 provider
+const frame = ethProvider('frame') // Connect to Frame
+frame.setChain(534352); // Scroll Mainnet
+// frame.setChain(534351);    // Scroll testnet on Sepolia
+// frame.setChain(11155111); // Sepolia
+
+
 
 async function main() {
-  const warrantCanary = await hre.ethers.deployContract("WarrantCanary");
+  const warrantCanary = await hre.ethers.getContractFactory("WarrantCanary");
+  const tx = await warrantCanary.getDeployTransaction()
+  tx.from = (await frame.request({ method: 'eth_requestAccounts' }))[0]
+  await frame.request({ method: 'eth_sendTransaction', params: [tx] })
 
   await warrantCanary.waitForDeployment();
 
